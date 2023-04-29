f1e54c7ebc9f08c1a527b5f9279ac76881c78070
OgabekToshpolatov <ogabektoshpolatov2002@gmail.com>
2023-04-29 06:56:22 +0500

    Added Project reference and assembly

---
+++ Bulky/Bulky.DataAccess/Data/AppDbContext.cs
@@ -0,0 +1,11 @@
+using Bulky.Models; 
+using Microsoft.EntityFrameworkCore;
+
+namespace Bulky.DataAccess.Data;
+
+public class AppDbContext:DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options):base(options) { }
+
+    public  DbSet<Category>? Categories { get; set; } 
+}
