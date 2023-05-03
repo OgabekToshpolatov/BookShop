8612d3335d54e956dd29147545c4c76d0d9cc80b
OgabekToshpolatov <ogabektoshpolatov2002@gmail.com>
2023-05-03 21:43:14 +0500

    added uRlImage

--- Bulky/BulkyWeb/Areas/Admin/Controllers/ProductController.cs
+++ Bulky/BulkyWeb/Areas/Admin/Controllers/ProductController.cs
@@ -12,10 +12,12 @@ namespace BulkyBookWeb.Areas.Admin.Controllers;
 public class ProductController : Controller
 {
     private readonly IUnitOfWork _unitOfWork;
+    private readonly IWebHostEnvironment _webHostEnvironment;
 
-    public ProductController(IUnitOfWork unitOfWork)
+    public ProductController(IUnitOfWork unitOfWork, IWebHostEnvironment webHostEnvironment)
     {
         _unitOfWork = unitOfWork;
+        _webHostEnvironment = webHostEnvironment;
     }
 
     public IActionResult Index()
@@ -45,6 +47,7 @@ public class ProductController : Controller
         {
             //update
             productVM.Product = _unitOfWork.Product.Get(u => u.Id == id);
+            Console.WriteLine("####################################" + productVM.Product.ImageUrl);
             return View(productVM);
         }
 
@@ -55,6 +58,20 @@ public class ProductController : Controller
     {
         if (ModelState.IsValid)
         {
+            string wwwRootPath = _webHostEnvironment.WebRootPath;
+            if(file != null)
+            {
+                string fileName = Guid.NewGuid().ToString() + Path.GetExtension(file.FileName);
+                string productPath = Path.Combine(wwwRootPath, @"images\product");
+
+                using (var fileStream = new FileStream(Path.Combine(productPath, fileName), FileMode.Create))
+                {
+                    file.CopyTo(fileStream);
+                }
+
+                productVM.Product.ImageUrl = @"images\product\" + fileName;
+            }
+
             _unitOfWork.Product.Add(productVM.Product);
             _unitOfWork.Save();
             TempData["success"] = "Product created successfully";
